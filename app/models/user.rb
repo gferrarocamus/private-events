dab1ac465288803408f3836ea935a662641f0d15
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-04-29 18:31:54 +0200

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@
 
 # User
 class User < ApplicationRecord
-  has_many :attendance, foreign_key: :event_attendee_id
+  has_many :attendance, foreign_key: :event_attendee_id, inverse_of: :attendances, dependent: :destroy
   has_many :attended_events, -> { distinct }, through: :attendance
-  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
+  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event', inverse_of: :creator, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
