195b634d17dde4dae70bd830b988341feef2303c
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-05-03 15:24:18 +0200

    move methods from helper to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,26 @@
 
 # ApplicationController
 class ApplicationController < ActionController::Base
-  include SessionsHelper
-  include EventsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    session[:user_id].nil? ? nil : User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !session[:user_id].nil?
+  end
 
   def logged_in
     return if logged_in?
 
     flash[:danger] = 'Please log in.'
     redirect_to signin_url
+  end
+
+  def user_attending
+    @event.event_attendees.exists?(current_user.id)
   end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,7 +2,4 @@
 
 # EventsHelper
 module EventsHelper
-  def user_attending
-    @event.event_attendees.exists?(current_user.id)
-  end
 end
